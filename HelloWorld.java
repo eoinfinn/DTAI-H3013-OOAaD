4548ea20eda72d4bd9ffbba9d20b2881ee4561d5
Mark Deegan <mark.deegan@tudublin.ie>
2023-10-19 18:06:42 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+Mark Deegan <br>
+20231019 1200 <br>
+HelloWorld <br>
+Sample to demonstrate basic java concepts
+*/
+public class HelloWorld
+{ // start declaration of the class HelloWorld
+	public static void main(String[] args)
+    	{ // start declaration of main method
+	System.out.println("Hello World!"); // standard Hello World! greeting
+	} // end declaration of main method
+} // end declaration of class HelloWorld
