017f596876a373402283f1eed3abc0fadfef8a85
Mark Deegan <mark.deegan@tudublin.ie>
2023-11-05 10:21:20 +0000

    Moving files and folders

---
+++ 00-CodeSamples/ex02/CollegeCourse.java
@@ -0,0 +1,14 @@
+public class CollegeCourse
+{	
+	public static void main(String[] args)
+	{
+		Student Shalom = new Student();
+			System.out.println("The object is: "+Shalom+"\n\n");
+		Student Eoin = new Student();
+			System.out.println("The object is: "+Eoin+"\n\n");
+		Student Armand = new Student();
+			System.out.println("The object is: "+Armand+"\n\n");
+
+		Lecturer Mark = new Lecturer();
+	}	
+}
---
+++ 00-CodeSamples/ex02/Person.java
@@ -0,0 +1,13 @@
+/** 
+Mark Deegan <br>
+20231025 1639 <br>
+Person class <br>
+Sample code to demonstrate class declaration and inheritance (extending classes) <br>
+*/
+public class Person
+{ // start declaration of public class Person
+	public static void main(String[] args)
+	{ // start declaration of the main method
+		System.out.println("Hello, I am a Person!"); // print a simple message
+	} // end declaration of the main method
+} // end declaration of class Person
